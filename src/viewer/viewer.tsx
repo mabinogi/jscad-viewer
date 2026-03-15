9adf502ae9f50e67db652ad4aba333548eb2e803
Stuart Herring <github@stuartherring.com>
2026-03-15 17:17:00 +1100

    minor improvements to display.
    Next is implementing mouse movement.

--- src/viewer/viewer.tsx
+++ src/viewer/viewer.tsx
@@ -12,6 +12,7 @@ import './viewer.css';
 export const Viewer = () => {
   const viewerRef = useRef<HTMLDivElement>(null);
   const mountedRef = useRef(false);
+  const updateRef = useRef(false);
 
   const entities = entitiesFromSolids({}, design.main({ color: [1, 0, 0], size: 100 }));
 
@@ -42,6 +43,15 @@ export const Viewer = () => {
         drawLines: drawCommands.drawLines,
         drawMesh: drawCommands.drawMesh,
       },
+        rendering: {
+          background: [0, 0, 0, 1],
+          lightDirection: [0.0, 0.0, 1.0],
+          lightPosition: [100.0, 100.0, 100.0],
+          ambientLightAmount: 0.5,
+          diffuseLightAmount: 0.0,
+          specularLightAmount: 0.0,
+          materialShininess: 1.0,
+        },      
       entities: [
         { // grid data
           // the choice of what draw command to use is also data based
@@ -50,24 +60,24 @@ export const Viewer = () => {
             show: true
           },
           size: [500, 500],
-          ticks: [25, 5]
-          // color: [0, 0, 1, 1],
-          // subColor: [0, 0, 1, 0.5]
+          ticks: [25, 5],
+          color: [0, 0, 1, 1],
+          subColor: [0, 0, 1, 0.5]
         },
         {
           visuals: {
             drawCmd: 'drawAxis',
             show: true
           },
-          size: 300
-          // alwaysVisible: false,
+          size: 300,
+          // alwaysVisible: true,
           // xColor: [0, 0, 1, 1],
           // yColor: [1, 0, 1, 1],
           // zColor: [0, 0, 0, 1]
         },
         ...entities
       ]
-    };
+    } as const;
 
     // prepare
     const render = prepareRender(options);
@@ -79,8 +89,17 @@ export const Viewer = () => {
       perspectiveCamera.update(camera, camera);
 
       render(options);
+      // window.requestAnimationFrame(updateAndRender);
     }
     window.requestAnimationFrame(updateAndRender);
+
+    window.addEventListener('resize', () => {
+      if (!viewerRef.current) return;
+      const width = window.innerWidth;
+      const height = window.innerHeight;
+      perspectiveCamera.setProjection(camera, camera, { width, height });
+      updateAndRender();
+    });
   }, []);
 
   return <div ref={viewerRef} className="viewer" id="viewer"></div>
